c9b043752b65054f1a40f3e7b8ea8d32d5e16b75
ueda-kenta <kosian.4141@gmail.com>
2020-09-02 14:11:26 +0000

    [start]follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  # フォロー関連(relationships)紐付け
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   attachment :profile_image, destroy: false
 
