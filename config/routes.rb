ccafbf2b0176e70be7da204fa5d3b79d7c08424f
ueda-kenta <kosian.4141@gmail.com>
2020-09-02 08:54:09 +0000

    [Add]comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
   	resource :favorites, only: [:create, :destroy]
+  	resource :book_comments, only: [:create, :destroy]
 end
   get 'home/about'
 end
