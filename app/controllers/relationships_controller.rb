25242979f2c23ca8bd4576100a556ecb2a7c5696
ueda-kenta <kosian.4141@gmail.com>
2020-09-03 02:14:09 +0000

    [fix]follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,15 @@
 class RelationshipsController < ApplicationController
 
-	before_action :set_user
+before_action :set_user
 
   def create
     following = current_user.follow(@user)
     if following.save
       flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @user
+      redirect_back(fallback_location: users_path)
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
+       redirect_back(fallback_location: users_path)
     end
   end
 
@@ -17,16 +17,18 @@ class RelationshipsController < ApplicationController
     following = current_user.unfollow(@user)
     if following.destroy
       flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
+      redirect_back(fallback_location: users_path)
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
+       redirect_back(fallback_location: users_path)
     end
   end
 
   private
-  def
-    @user = User.find(params[:relationship][:follow_id])
+  def set_user
+    # params.require(:user).permit(:follow_id)
+    @user = User.find(params[:follow_id])
+
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   resources :users,only: [:show,:index,:edit,:update]
+   resources :relationships, only: [:create, :destroy]
   resources :books do
   	resource :favorites, only: [:create, :destroy]
   	resource :book_comments, only: [:create, :destroy]
